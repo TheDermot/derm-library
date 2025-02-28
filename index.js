0daf75e9eaf8801ce6125d802b641b50342e97d3
TheDermot <88741983+TheDermot@users.noreply.github.com>
2025-02-27 19:33:03 -0500

    Set borders and display flex to elements

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello");
+const myLibrary = [];
+
+//book constructor
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+//create book
+function addBooktoLibrary() {}
